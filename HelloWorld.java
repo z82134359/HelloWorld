ee74d7bd2ba6e60e66d2a382f841af32a3d79cc3
z82134359 <821634359@qq.com>
2017-04-04 15:47:33 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+helloworld
+*/
+//Create a "HelloWorld"class
+public class HelloWorld {
+	//"Main" method
+	public static void main(String[] args) {
+		//export "HelloWorld"
+		System.out.println("HelloWorld");
+	}
+}
